9864323b71246ca73bd9e32fedcc20e2101cc946
Jost Schulte <jost.schulte@protonmail.com>
2023-10-24 06:01:58 -0600

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.follows = "holonix/nixpkgs";
+
+    versions.url = "github:holochain/holochain?dir=versions/weekly";
+    holonix.url = "github:holochain/holochain";
+    holonix.inputs.versions.follows = "versions";
+    holonix.inputs.holochain.url = "github:holochain/holochain/holochain-0.3.0-beta-dev.22";
+  };
+
+  outputs = inputs@{ holonix, ... }:
+    holonix.inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      # provide a dev shell for all systems that the holonix flake supports
+      systems = builtins.attrNames holonix.devShells;
+
+      perSystem = { config, system, pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [ holonix.devShells.${system}.holochainBinaries ];
+            packages = with pkgs; [
+              # add further packages from nixpkgs
+              nodejs
+            ];
+          };
+        };
+    };
+}
